906cc4182a242aed0db056b54334d7075661ad56
Kevin Jake Casala <kevin_jake_04@yahoo.com>
2022-01-06 18:53:08 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import "./App.css";
+import TransactionList from "./components/TransactionList";
+import AddTransaction from "./components/AddTransaction";
 
 const App = (props) => {
-  return <div></div>;
+  return (
+    <div>
+      <Container>
+        <Header />
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </Container>
+    </div>
+  );
 };
 
 export default App;
